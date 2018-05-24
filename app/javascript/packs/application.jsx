6829d6ed4922d189f3a603eea02b5549316b753e
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-05-24 14:58:15 +0300

    Add react application

---
+++ app/javascript/packs/application.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+import 'semantic-ui-css/semantic.css'
+import './index.css'
+import App from './make-a-drink'
+
+ReactDOM.render(<App />, document.getElementById('root'))
