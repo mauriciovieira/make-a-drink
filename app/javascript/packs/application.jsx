fe8e31685c39bd3255f4836449574639b82ac1b8
Mauricio Vieira <mauricio@mauriciovieira.net>
2018-05-24 17:23:55 +0300

    Load stylesheets in application layout

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 import 'semantic-ui-css/semantic.css'
-import './index.css'
+import './application.css'
 import App from './make-a-drink'
 
 ReactDOM.render(<App />, document.getElementById('root'))
